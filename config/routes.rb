347332a220b7860c5fff839b2b90557676ce2734
Tretent <pasquali.roberto@gmail.com>
2026-01-21 12:34:31 +0100

    add authentication boilerplate

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,8 @@ Rails.application.routes.draw do
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
+  resource :session
+  resources :passwords, param: :token
+
   resources :games, only: %i[index]
 end
