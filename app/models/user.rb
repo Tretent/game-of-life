75252a5e5b778bfa9e6d6c8456279facecac05c5
Tretent <pasquali.roberto@gmail.com>
2026-01-22 16:26:41 +0100

    update user model to accept email confirmation on sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ApplicationRecord
   has_many :games, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  generates_token_for :email_confirmation, expires_in: 24.hours do
+    confirmed_at
+  end
+
+  def confirmed?
+    confirmed_at.present?
+  end
+
+  def confirm!
+    update!(confirmed_at: Time.current)
+  end
 end
