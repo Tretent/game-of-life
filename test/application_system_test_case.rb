6dca791339d866117edf75578cf3b64ac1c0723a
Tretent <pasquali.roberto@gmail.com>
2026-01-23 12:07:39 +0100

    add initial system test support

---
+++ test/application_system_test_case.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+
+  def sign_in_as(user, password: "password")
+    visit new_session_path
+    fill_in "Email", with: user.email_address
+    fill_in "Password", with: password
+    click_button "Sign in"
+    assert_no_selector "h1", text: "Sign in"
+  end
+end
